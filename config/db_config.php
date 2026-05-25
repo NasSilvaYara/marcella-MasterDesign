ad0eaf0ca1b064340029e6b8546744523bc34019
Yara da Silva <Nassilvayara@gmail.com>
2026-05-24 21:07:19 -0300

    fix: atualiza conexão do banco

--- config/db_config.php
+++ config/db_config.php
@@ -1,16 +1,28 @@
 <?php
-$host    = getenv('DB_HOST');
-$db      = getenv('DB_NAME');
-$user    = getenv('DB_USER');
-$pass    = getenv('DB_PASS');
+
+$host = "sql10.freesqldatabase.com";
+$db   = "sql10828153";
+$user = "sql10828153";
+$pass = "rfzuwdQD6A";
 $charset = "utf8mb4";
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=$charset", $user, $pass);
+
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$db;charset=$charset",
+        $user,
+        $pass
+    );
+
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+    echo "Conectado com sucesso!";
+
 } catch (PDOException $e) {
+
     die("Erro na conexão com o banco: " . $e->getMessage());
+
 }
 
 $admin_email = "golcalvesmarcella@gmail.com";
