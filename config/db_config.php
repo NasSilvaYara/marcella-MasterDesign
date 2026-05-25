907510f7f42a6a9f50343083c3a10433fcfda842
Yara da Silva <Nassilvayara@gmail.com>
2026-05-24 21:17:17 -0300

    fix: atualiza conexão do banco

--- config/db_config.php
+++ config/db_config.php
@@ -17,8 +17,6 @@ try {
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-    echo "Conectado com sucesso!";
-
 } catch (PDOException $e) {
 
     die("Erro na conexão com o banco: " . $e->getMessage());
